dbbce58526976d89dee97524ebacd245ceb0e751
Luca Capra <luca.capra@gmail.com>
2020-04-26 18:43:34 +0200

    added click handler

--- content.js
+++ content.js
@@ -15,14 +15,27 @@
       // eg update injected elements,
       // add event listeners or logic to connect to other parts of the app
 
+
+      if (exists()) {
+        return
+      }
+
       var div = document.createElement('div');
       div.id = id
       const reflink = document.location.toString()
       div.innerHTML = html.replace("{referer}", encodeURIComponent(reflink))
 
-      if (!exists()) {
-        document.body.appendChild(div);
-      }
+      div.addEventListener("click", (ev) => {
+        console.log("click", ev)
+        if (ev.target.nodeName === "A") {
+          return true
+        }
+
+        div.parentNode.removeChild(div)
+        ev.preventDefault()
+      })
+
+      document.body.appendChild(div);
 
     }).catch(err => {
       console.error("Failed to load /content.html")
